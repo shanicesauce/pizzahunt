274e13eab2b786dffad18343a06e3c4547de1ec5
shanicesauce <shanicesauceda@gmail.com>
2022-11-14 14:05:17 -0600

    validation added

--- models/Comment.js
+++ models/Comment.js
@@ -8,10 +8,14 @@ const ReplySchema = new Schema({
         default: () => new Types.ObjectId
     },
     replyBody: {
-        type: String
+        type: String,
+        required: 'You need to provide a reply!',
+        trim: true
     },
     writtenBy: {
         type: String,
+        required: 'You need to provide your name!',
+        trim: true
     },
     createdAt: {
         type: Date,
@@ -28,9 +32,13 @@ const ReplySchema = new Schema({
 const CommentSchema = new Schema({
     writtenBy: {
         type: String,
+        required: 'You need to provide your name!',
+        trim: true
     },
     commentBody: {
-        type: String
+        type: String,
+        required: 'You need to provide a comment!',
+        trim: true
     },
     createdAt: {
         type: Date,
